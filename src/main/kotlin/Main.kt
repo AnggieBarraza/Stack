85a5b2a41b571205cfb97400fc57b5a513860782
AnggieBarraza <angelica.r.barraza@gmail.com>
2024-12-03 17:03:22 -0600

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+package org.example
+
+fun main() {
+    val list = AnggieStack()
+}
+data class Node (internal var info: String, internal var next : String? = null){
+
+}
+
+open class AnggieStack {
+
+}
